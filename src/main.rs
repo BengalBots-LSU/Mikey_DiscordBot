6299b354a393ee346d5972975c025a7b37100691
AZapie <azaier03@gmail.com>
2024-11-11 15:58:50 -0600

    Refactored code to take in token from .env file and no longer relies on user to pass token as parameter.

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,41 @@
-/*
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
+use std::env;
 
- use std::env;
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::prelude::*;
 
- use serenity::{
-     async_trait,
-     model::{channel::Message, gateway::Ready},
-     prelude::*,
- };
- 
- const HELP_MESSAGE: &str = "
- Hello there, Human!
- 
- You have summoned me. Let's see about getting you what you need.
- 
- ❓ Need technical help?
- ➡️ Post in the <#CHANNEL_ID> channel and other humans will assist you.
- 
- ❓ Looking for the Code of Conduct?
- ➡️ Here it is: <https://opensource.facebook.com/code-of-conduct>
- 
- ❓ Something wrong?
- ➡️ You can flag an admin with @admin
- 
- I hope that resolves your issue!
- 
- — HelpBot 🤖
- ";
- 
- const HELP_COMMAND: &str = "!help";
- 
- struct Handler;
- 
- #[async_trait]
- impl EventHandler for Handler {
-     async fn message(&self, ctx: Context, msg: Message) {
-         if msg.content == HELP_COMMAND {
-             if let Err(why) = msg.channel_id.say(&ctx.http, HELP_MESSAGE).await {
-                 println!("Error sending message: {:?}", why);
-             }
-         }
-     }
- 
-     async fn ready(&self, _: Context, ready: Ready) {
-         println!("{} is connected!", ready.user.name);
-     }
- }
- 
- #[tokio::main]
- async fn main() {
-     let token = env::var("DISCORD_TOKEN")
-         .expect("Expected a token in the environment");
- 
-     let mut client = Client::new(&token)
-         .event_handler(Handler)
-         .await
-         .expect("Err creating client");
- 
-     if let Err(why) = client.start().await {
-         println!("Client error: {:?}", why);
-     }
- }
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!ping" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("Error sending message: {why:?}");
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    // Login with a bot token from the environment
+    dotenvy::dotenv();
+
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    // Set gateway intents, which decides what events the bot will be notified about
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    // Create a new instance of the Client, logging in as a bot.
+    let mut client =
+        Client::builder(&token, intents).event_handler(Handler).await.expect("Err creating client");
+
+    // Start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("Client error: {why:?}");
+    }
+}
+
+//  #ISSUE: after building the program. Trying to pass the token as a variable into the executable via the command line fails. 
