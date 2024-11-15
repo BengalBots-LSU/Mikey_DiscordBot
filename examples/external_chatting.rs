1ec72ffeccd10b440f6787e3b2c3d2b9f40eb8e3
PrudhviBoggavarapu <codingpabs@gmail.com>
2024-11-15 01:47:04 -0600

    Made alot of things alot nicer, see external_chatting

--- examples/external_chatting.rs
+++ examples/external_chatting.rs
@@ -1,100 +1,34 @@
-use ::serenity::all::{Cache, CreateMessage};
-use poise::{serenity_prelude as serenity, Command};
-use serenity::prelude::Context as SerenityContext;
-use tokio::sync::{mpsc, Mutex};
-
-use std::{env, future::IntoFuture, sync::Arc};
-
-struct Data {} // User data, which is stored and accessible in all command invocations
-type Error = Box<dyn std::error::Error + Send + Sync>;
-type Context<'a> = poise::Context<'a, Data, Error>;
-
-/// A command that responds with pong!
-#[poise::command(slash_command)]
-async fn ping(ctx: Context<'_>) -> Result<(), Error> {
-    ctx.say("Pong!").await?;
-    Ok(())
-}
-
-async fn get_rss_feed() -> Vec<String> {
-    let content = reqwest::get("http://rss.cnn.com/rss/cnn_topstories.rss")
-        .await
-        .unwrap()
-        .bytes()
-        .await
-        .unwrap();
-    let channel = rss::Channel::read_from(&content[..]).unwrap();
-    channel
-        .items
-        .into_iter()
-        .map(|x| x.title().unwrap().to_string())
-        .collect()
-}
-
-#[poise::command(slash_command)]
-async fn cnn_test(ctx: Context<'_>) -> Result<(), Error> {
-    ctx.say(get_rss_feed().await.last_chunk::<3>().unwrap().join(" "))
-        .await?;
-    Ok(())
-}
+use ::serenity::all::CreateMessage;
+use mikey::{
+    create_client, send_message_to_channel, start_discord_client, SendDataToDiscord,
+    SendToDiscordMpsc,
+};
+use poise::serenity_prelude as serenity;
+use serenity::model::id::ChannelId;
+use tokio::sync::mpsc;
 
 #[tokio::main]
 async fn main() {
-    let _ = dotenvy::dotenv();
+    // Create the client and shared context.
+    let (client, senerity_context) = create_client().await;
 
-    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
-    let intents = serenity::GatewayIntents::non_privileged();
-    let shared_ctx: Arc<Mutex<Option<SerenityContext>>> = Arc::new(Mutex::new(None));
-    let framework = poise::Framework::builder()
-        .options(poise::FrameworkOptions {
-            commands: vec![ping(), cnn_test()],
-            ..Default::default()
-        })
-        .setup({
-            let shared_ctx = Arc::clone(&shared_ctx);
-            move |ctx, _ready, framework| {
-                Box::pin(async move {
-                    // Store `serenity::Context` in `shared_ctx`
-                    {
-                        let mut ctx_lock = shared_ctx.lock().await;
-                        *ctx_lock = Some(ctx.clone());
-                    }
-                    poise::builtins::register_globally(ctx, &framework.options().commands).await?;
-                    Ok(Data {})
-                })
-            }
-        })
-        .build();
+    let (message_buffer_send, message_buffer_rec) = mpsc::channel::<SendDataToDiscord>(50);
 
-    let client = serenity::ClientBuilder::new(token, intents)
-        .framework(framework)
-        .await;
-
-    let mut client = client.unwrap();
-
-    tokio::join!(
-        async { client.start().await.unwrap() },
-        loop_outside(shared_ctx)
+    // Use tokio::join! to run multiple async tasks concurrently.
+    let (_discord_result, _message_result, _scream_result) = tokio::join!(
+        async { start_discord_client(client.unwrap()).await },
+        async { send_message_to_channel(senerity_context.clone(), message_buffer_rec).await },
+        async { scream_from_outside(message_buffer_send).await }
     );
 }
-use serenity::model::id::ChannelId;
 
-async fn loop_outside(global_ctx: Arc<Mutex<Option<SerenityContext>>>) {
+async fn scream_from_outside(send: SendToDiscordMpsc) {
     loop {
-        let builder = CreateMessage::new().content("AAAAAAAAAAAAAAAAAAA");
-        let ctx = global_ctx.lock().await;
-
-        if let Some(ctx) = ctx.clone() {
-            let message = ChannelId::new(1102864318233595934)
-                .send_message(ctx.clone(), builder)
-                .await;
-            if let Err(why) = message {
-                eprintln!("Error sending message: {why:?}");
-            };
-        } else {
-            println!("Waiting...")
-        }
-
+        let send_message = SendDataToDiscord {
+            message: CreateMessage::new().content("AAAAAAAAAAAAAAAAAAA"),
+            channel: ChannelId::new(1102864318233595934),
+        };
+        send.send(send_message).await.unwrap();
         tokio::time::sleep(std::time::Duration::from_secs(1)).await;
     }
 }
---
+++ src/lib.rs
@@ -0,0 +1,118 @@
+use ::serenity::all::CreateMessage;
+use poise::serenity_prelude as serenity;
+use serenity::model::id::ChannelId;
+use serenity::prelude::Context as SerenityContext;
+use std::{env, sync::Arc};
+use tokio::sync::Mutex;
+pub type SendToDiscordMpsc = tokio::sync::mpsc::Sender<SendDataToDiscord>;
+
+pub struct SendDataToDiscord {
+    pub message: CreateMessage,
+    pub channel: ChannelId,
+}
+pub struct Data {} // User data, which is stored and accessible in all command invocations
+pub type Error = Box<dyn std::error::Error + Send + Sync>;
+pub type Context<'a> = poise::Context<'a, Data, Error>;
+
+/// A command that responds with pong!
+#[poise::command(slash_command)]
+async fn ping(ctx: Context<'_>) -> Result<(), Error> {
+    ctx.say("Pong!").await?;
+    Ok(())
+}
+
+pub async fn get_rss_feed() -> Vec<String> {
+    let content = reqwest::get("http://rss.cnn.com/rss/cnn_topstories.rss")
+        .await
+        .unwrap()
+        .bytes()
+        .await
+        .unwrap();
+    let channel = rss::Channel::read_from(&content[..]).unwrap();
+    channel
+        .items
+        .into_iter()
+        .map(|x| x.title().unwrap().to_string())
+        .collect()
+}
+
+#[poise::command(slash_command)]
+async fn cnn_test(ctx: Context<'_>) -> Result<(), Error> {
+    ctx.say(get_rss_feed().await.last_chunk::<3>().unwrap().join(" "))
+        .await?;
+    Ok(())
+}
+
+pub async fn start_discord_client(mut client: serenity::Client) {
+    client.start().await.unwrap();
+}
+
+pub async fn create_client() -> (
+    Result<serenity::Client, serenity::Error>,
+    Arc<serenity::prelude::Mutex<Option<serenity::prelude::Context>>>,
+) {
+    let _ = dotenvy::dotenv();
+    let shared_ctx: Arc<Mutex<Option<SerenityContext>>> = Arc::new(Mutex::new(None));
+
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    let intents = serenity::GatewayIntents::non_privileged();
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![ping(), cnn_test()],
+            ..Default::default()
+        })
+        .setup({
+            let shared_ctx = Arc::clone(&shared_ctx);
+            move |ctx, _ready, framework| {
+                Box::pin(async move {
+                    // Store `serenity::Context` in `shared_ctx`
+                    {
+                        let mut ctx_lock = shared_ctx.lock().await;
+                        *ctx_lock = Some(ctx.clone());
+                    }
+                    poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                    Ok(Data {})
+                })
+            }
+        })
+        .build();
+
+    let client = serenity::ClientBuilder::new(token, intents)
+        .framework(framework)
+        .await;
+    (client, shared_ctx)
+}
+
+pub async fn scream_from_outside(send: SendToDiscordMpsc) {
+    loop {
+        let send_message = SendDataToDiscord {
+            message: CreateMessage::new().content("AAAAAAAAAAAAAAAAAAA"),
+            channel: ChannelId::new(1102864318233595934),
+        };
+        send.send(send_message).await.unwrap();
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    }
+}
+
+pub async fn send_message_to_channel(
+    global_ctx: Arc<Mutex<Option<SerenityContext>>>,
+    mut rec: tokio::sync::mpsc::Receiver<SendDataToDiscord>,
+) {
+    loop {
+        let dirty = rec.recv().await.unwrap();
+
+        let channel_id = dirty.channel;
+
+        let builder = dirty.message;
+        let ctx = global_ctx.lock().await;
+
+        if let Some(ctx) = ctx.clone() {
+            let message = channel_id.send_message(ctx.clone(), builder).await;
+            if let Err(why) = message {
+                eprintln!("Error sending message: {why:?}");
+            };
+        } else {
+            println!("Waiting...")
+        }
+    }
+}
